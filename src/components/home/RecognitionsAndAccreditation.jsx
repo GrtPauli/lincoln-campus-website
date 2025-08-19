c5ad8a75462c1c6edb71e8ed81d2da9dd3c67127
Dandokku <jesulobadaniel1@gmail.com>
2025-08-19 14:49:29 +0100

    Ui: added recognitions and accreditations component

---
+++ src/components/home/RecognitionsAndAccreditation.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RecognitionsAndAccreditation() {
+  return (
+    <div>RecognitionsAndAccreditation</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeHeroSection from '../components/home/Hero'
+import RecognitionsAndAccreditation from '../components/home/RecognitionsAndAccreditation'
 
 export default function HomePage() {
   return (
     <div>
-      <HomeHeroSection/>
+      <HomeHeroSection />
+      <RecognitionsAndAccreditation />
     </div>
   )
 }

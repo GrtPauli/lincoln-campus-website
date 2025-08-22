bcfd62a794594990e2e5a8efc82529f8202ada54
Paul Andrew <paulandrew2004@gmail.com>
2025-08-22 08:31:10 +0100

    Updated navbar, worked on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./pages/NotFound";
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
 import HomePage from "./pages/Home";
 
 // Welcome to LUC Pages
@@ -10,8 +8,8 @@ import About from "./pages/About";
 import FacilitesAndServices from "./pages/FacilitesAndServices";
 
 // Research and Conferences Pages
-import Introduction from "./pages/researchandconferences/Introduction";
-import ResearchProject from "./pages/researchandconferences/ResearchProject";
+import Introduction from "./pages/research-and-conferences/Introduction";
+import ResearchProject from "./pages/research-and-conferences/ResearchProject";
 
 // Contact Pages
 import Contact from "./pages/Contact";
@@ -20,7 +18,6 @@ function App() {
   return (
     <Router>
       <main>
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact-us" element={<Contact />} />
@@ -45,7 +42,6 @@ function App() {
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
-      <Footer />
     </Router>
   );
 }
--- src/pages/FacilitesAndServices.jsx
+++ src/pages/FacilitesAndServices.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import FacilitiesAndServicesContent from "../components/facilitiesandservices/FacilitiesAndServicesContent";
+import FacilitiesAndServicesContent from "../components/facilities-and-services/FacilitiesAndServicesContent";
 import Hero from "../components/common/ui/Hero";
+import MainLayout from "../layouts/MainLayout";
 
 export default function FacilitesAndServices() {
   return (
-    <div>
-      <Hero
-        title="Facilities and Services"
-        backgroundImage="https://plus.unsplash.com/premium_photo-1693035341269-8bbcfe73ff5a?w=500&auto=format&fit=crop&q=60"
-        height="h-[200px] md:h-[200px] lg:h-[300px]"
-        overlayOpacity="bg-black/10"
-      />
-      <FacilitiesAndServicesContent />
-    </div>
+    <MainLayout>
+      <div>
+        <Hero
+          title="Facilities and Services"
+          backgroundImage="https://plus.unsplash.com/premium_photo-1693035341269-8bbcfe73ff5a?w=500&auto=format&fit=crop&q=60"
+          height="h-[200px] md:h-[200px] lg:h-[300px]"
+          overlayOpacity="bg-black/10"
+        />
+        <FacilitiesAndServicesContent />
+      </div>
+    </MainLayout>
   );
 }

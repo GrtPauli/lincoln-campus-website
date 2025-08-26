9fcd0c1cce2c9bf98c6c91f3452c1b9a75af0f61
Paul Andrew <96921623+GrtPauli@users.noreply.github.com>
2025-08-26 12:40:29 +0100

    Revert "fixed the merge conflicts issue and i added the accreditations page "

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NotFoundPage from "./pages/NotFound";
 import HomePage from "./pages/Home";
-
-// Welcome to LUC Pages
-import About from "./pages/About";
-import FacilitesAndServices from "./pages/FacilitesAndServices";
-import RecognitionsAndAccreditation
- from "./components/home/RecognitionsAndAccreditation";
-
-// Research and Conferences Pages
-import Introduction from "./pages/research-and-conferences/Introduction";
-import ResearchProject from "./pages/research-and-conferences/ResearchProject";
-
-// Contact Pages
-import Contact from "./pages/Contact";
+import NotFoundPage from "./pages/NotFound";
 
 function App() {
   return (
@@ -22,25 +8,6 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/contact-us" element={<Contact />} />
-
-          {/* Welcome to LUC Pages */}
-          <Route path="/about-us" element={<About />} />
-          <Route
-            path="/facilities-and-services"
-            element={<FacilitesAndServices />}
-          />
-         <Route path="/recognitions" element={<RecognitionsAndAccreditation/>}/>
-          {/* Research and Development Pages */}
-          <Route
-            path="/research-and-conferences/introduction"
-            element={<Introduction />}
-          />
-          <Route
-            path="/research-and-conferences/research-project"
-            element={<ResearchProject />}
-          />
-
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
